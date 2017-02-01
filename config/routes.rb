29e9fc737dffbb7a9107c7fec34ecb0f7ab3b2bd
Travis Brace <TravisBrace@gmail.com>
2017-01-31 23:16:44 -0500

    updated root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/tags' => 'tags#index'
+
+
+  root 'tags#index'
   get '/tags/new' => 'tags#new'
   post 'tags' => 'tags#create'
   get '/tags/:id' => 'tags#show', as: :tag
